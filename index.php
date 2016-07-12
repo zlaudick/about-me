1ecf4da126eefe38a21c258d9aa2c2ef11acd8b7
Zac Laudick <zlaudick@nmsu.edu>
2016-07-12 16:46:10 -0600

    added lists scaffolding

--- index.php
+++ index.php
@@ -2,9 +2,26 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<title></title>
+		<title>Zac Laudick</title>
 	</head>
 	<body>
-		
+		<header>
+			<h1>Hello World, I'm Zac!</h1>
+		</header>
+
+		<!-- Main content here -->
+		<main>
+			<!-- List of Interests -->
+			<h2>Interests</h2>
+			<ul></ul>
+
+			<!-- List of Education -->
+			<h2>Education</h2>
+			<ul></ul>
+
+			<!-- List of places i've lived -->
+			<h2>Places I've Lived</h2>
+			<ul></ul>
+		</main>
 	</body>
 </html>
