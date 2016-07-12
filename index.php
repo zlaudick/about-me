9d3e20458f7feaaac5deb6d0fbdddda30fe43d8c
Zac Laudick <zlaudick@nmsu.edu>
2016-07-12 16:56:41 -0600

    completed Interests, Education, and Places I've Lived lists

--- index.php
+++ index.php
@@ -13,15 +13,32 @@
 		<main>
 			<!-- List of Interests -->
 			<h2>Interests</h2>
-			<ul></ul>
+			<ul>
+				<li>Coding</li>
+				<li>Poker</li>
+				<li>Golf</li>
+				<li>Video Games</li>
+				<li>ABQ Breweries</li>
+			</ul>
 
 			<!-- List of Education -->
 			<h2>Education</h2>
-			<ul></ul>
+			<ul>
+				<li>La Cueva High School, Albuquerque, NM</li>
+				<li>New Mexico State University, Las Cruces, NM</li>
+				<li>CNM STEMulus Deep Dive Coding Bootcamp, Albuquerque, NM. <em>current student</em></li>
+			</ul>
 
 			<!-- List of places i've lived -->
 			<h2>Places I've Lived</h2>
-			<ul></ul>
+			<ul>
+				<li>Oklahoma City, OK</li>
+				<li>Albuquerque, NM - <em>current</em></li>
+				<li>Las Cruces, NM</li>
+				<li>Dallas, TX</li>
+				<li>Rosarito, Mexico</li>
+				<li>Napa, CA</li>
+			</ul>
 		</main>
 	</body>
 </html>
