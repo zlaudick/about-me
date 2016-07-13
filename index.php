622c2ebac18acfed7a5b0cf1cfb5c2376b75dbcc
Zac Laudick <zlaudick@nmsu.edu>
2016-07-13 08:47:21 -0600

    added favicon seven-two and edited font styles

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
+
+		<!--Favicon-->
+		<link type="image/ico" href="images/seven-two.jpeg" rel="shortcut icon" />
 		<title>Zac Laudick</title>
 	</head>
 	<body>
 		<header>
-			<h1>Hello World, I'm Zac!</h1>
+			<h1 style="font-family:Arial">Hello World, I'm Zac!</h1>
 		</header>
 
 		<!-- Main content here -->
 		<main>
 			<!-- List of Interests -->
-			<h2>Interests</h2>
-			<ul>
+			<h2 style="font-family:Impact">Interests</h2>
+			<ul style="font-family:Arial">
 				<li>Coding</li>
 				<li>Poker</li>
 				<li>Golf</li>
@@ -22,16 +25,16 @@
 			</ul>
 
 			<!-- List of Education -->
-			<h2>Education</h2>
-			<ul>
+			<h2 style="font-family:cursive">Education</h2>
+			<ul style="font-family:Arial">
 				<li>La Cueva High School, Albuquerque, NM</li>
 				<li>New Mexico State University, Las Cruces, NM</li>
-				<li>CNM STEMulus Deep Dive Coding Bootcamp, Albuquerque, NM. <em>current student</em></li>
+				<li>CNM STEMulus Deep Dive Coding Bootcamp, Albuquerque, NM. - <em>current student</em></li>
 			</ul>
 
 			<!-- List of places i've lived -->
-			<h2>Places I've Lived</h2>
-			<ul>
+			<h2 style="font-family:fantasy">Places I've Lived</h2>
+			<ul style="font-family:Arial">
 				<li>Oklahoma City, OK</li>
 				<li>Albuquerque, NM - <em>current</em></li>
 				<li>Las Cruces, NM</li>
